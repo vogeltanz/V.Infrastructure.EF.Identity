99bab228af64fce82052a02a82a0f41676da427b
FunTom <vogeltanz@utb.cz>
2022-06-27 16:04:10 +0200

    IDisposable implemented in RepositoryIdentity and RepositoryIdentityUR

--- V.Infrastructure.EF.Identity.Base/RepositoryIdentity.cs
+++ V.Infrastructure.EF.Identity.Base/RepositoryIdentity.cs
@@ -5,13 +5,32 @@ using V.Infrastructure.EF.Base;
 
 namespace V.Infrastructure.EF.Identity.Base
 {
-    public class RepositoryIdentity<T, TKey, TIdentityKey> : Repository<T, TKey> where T : class, IEntity<TKey> where TIdentityKey : IEquatable<TIdentityKey>
+    public class RepositoryIdentity<T, TKey, TIdentityKey> : Repository<T, TKey>, IDisposable where T : class, IEntity<TKey> where TIdentityKey : IEquatable<TIdentityKey>
     {
         protected IdentityDbContext<IdentityUser<TIdentityKey>, IdentityRole<TIdentityKey>, TIdentityKey> dbContext;
 
         public RepositoryIdentity(IdentityDbContext<IdentityUser<TIdentityKey>, IdentityRole<TIdentityKey>, TIdentityKey> dbContext) : base(dbContext.Set<T>())
         {
             this.dbContext = dbContext;
+        }
+
+
+
+        private bool _disposed;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+                if (disposing)
+                {
+                    dbContext?.Dispose();
+                }
+            _disposed = true;
         }
     }
 }
--- V.Infrastructure.EF.Identity.Base/RepositoryIdentityUR.cs
+++ V.Infrastructure.EF.Identity.Base/RepositoryIdentityUR.cs
@@ -5,13 +5,32 @@ using V.Infrastructure.EF.Identity.Base.Entities;
 
 namespace V.Infrastructure.EF.Identity.Base
 {
-    public class RepositoryIdentityUR<T, TKey, TIdentityKey> : Repository<T, TKey> where T : class, IEntity<TKey> where TIdentityKey : IEquatable<TIdentityKey>
+    public class RepositoryIdentityUR<T, TKey, TIdentityKey> : Repository<T, TKey>, IDisposable where T : class, IEntity<TKey> where TIdentityKey : IEquatable<TIdentityKey>
     {
         protected IdentityDbContext<User<TIdentityKey>, Role<TIdentityKey>, TIdentityKey> dbContext;
 
         public RepositoryIdentityUR(IdentityDbContext<User<TIdentityKey>, Role<TIdentityKey>, TIdentityKey> dbContext) : base(dbContext.Set<T>())
         {
             this.dbContext = dbContext;
+        }
+
+
+
+        private bool _disposed;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+                if (disposing)
+                {
+                    dbContext?.Dispose();
+                }
+            _disposed = true;
         }
     }
 }
